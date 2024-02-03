4a0a8ab8854d56ab549016687c7ff6e014939f59
bingus-bongo <100236235+bingus-bongo@users.noreply.github.com>
2024-02-02 18:03:32 -0600

    Done

--- mood.js
+++ mood.js
@@ -5,11 +5,17 @@ function runSearch() {
 
   // TODO: Clear the results pane before you run a new search
 
+  resultsPane = document.body.querySelector("#resultsImageContainer");
+  resultsPane.innerHTML = '';
+  suggested = document.body.querySelector('.suggestions ul');
+  suggested.innerHTML = '';
   openResultsPane();
 
   // TODO: Build your query by combining the bing_api_endpoint and a query attribute
   //  named 'q' that takes the value from the search bar input field.
 
+  input = document.body.querySelector("#searchInput").value;
+  query = `${bing_api_endpoint}?q=${encodeURIComponent(input)}`;
   let request = new XMLHttpRequest();
 
   // TODO: Construct the request object and add appropriate event listeners to
@@ -26,7 +32,63 @@ function runSearch() {
   //
   // request.setRequestHeader("Ocp-Apim-Subscription-Key", bing_api_key);
 
+  request.open('GET', query, true);
+  request.responseType = 'json';
+  request.setRequestHeader("Ocp-Apim-Subscription-Key", bing_api_key);
+  
+  request.addEventListener('load', function(event) {
+    let response = request.response;
+    console.log(response);
+
+    if (response.relatedSearches && response.relatedSearches.length > 0) {
+      response.relatedSearches.slice(0, 6).forEach(searchTerm => {
+        console.log(searchTerm.text);
+        let newSuggestion = document.createElement('li');
+        newSuggestion.textContent = searchTerm.text;
+
+        suggested.appendChild(newSuggestion);
+
+        newSuggestion.addEventListener('click', function(event) {
+          document.body.querySelector("#searchInput").value = newSuggestion.textContent;
+          runSearch();
+        })
+      })
+    }
+  
+    if (response.value && response.value.length > 0) {
+        response.value.slice(0, 10).forEach(image => {
+        let img = document.createElement('img');
+        img.src = image.contentUrl;
+  
+        let div = document.createElement('div');
+        div.className = 'resultImage';
+        div.appendChild(img);
+  
+        resultsPane.appendChild(div);
+
+        img.addEventListener('click', function(event) {
+          let image = event.target;
+          console.log(image);
+
+          if (image) {
+            let img = document.createElement('img');
+            img.src = image.src;
+            board = document.body.querySelector("#board")
+            div = document.createElement('div');
+            div.className = 'savedImage';
+            div.appendChild(img);
+            board.appendChild(div);
+          }
+        });
+      });
+    } else {
+      resultsPane.innerHTML = 'No results found.';
+    }
+  });
+
   // TODO: Send the request
+
+  request.send();
 
   return false;  // Keep this; it keeps the browser from sending the event
                   // further up the DOM chain. Here, we don't want to trigger
